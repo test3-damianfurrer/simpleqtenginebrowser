12a066fd14d420d69fc0f20c2a249d82d76567d6
master <test3@damianfurrer.ch>
2022-10-29 13:32:31 +0200

    Added Context to "xdg-open" links. USefull for downloading linked stuff or opening another instance through a custom url handler script Changed default UA to a valid Desktop UA

--- qtw.cpp
+++ qtw.cpp
@@ -32,7 +32,8 @@ int main(int argc, char *argv[])
 	return 1;
     QString str (QCoreApplication::arguments().at(1));
     MainWindow *mw = new MainWindow(QUrl(str),&pwd);
-    mw->setUAString(QString("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"));
+//    mw->setUAString(QString("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"));
+    mw->setUAString(QString("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 OPR/91.0.4516.77"));
     mw->show();
 
     int ret=0;
--- qtwengineview.cpp
+++ qtwengineview.cpp
@@ -2,6 +2,7 @@
 #include <QApplication>
 #include <QMenu>
 #include <QWebEngineProfile>
+#include <QDesktopServices>
 
 WebEngineView::WebEngineView(QString *pwd, QWidget *parent = Q_NULLPTR)
 {
@@ -10,8 +11,30 @@ WebEngineView::WebEngineView(QString *pwd, QWidget *parent = Q_NULLPTR)
     profilepath+="/profile";
     setPage(new QWebEnginePage(new QWebEngineProfile(profilepath)));
     settings()->setAttribute(QWebEngineSettings::FullScreenSupportEnabled, true);
-    settings()->setAttribute(QWebEngineSettings::PluginsEnabled, true);
+//    settings()->setAttribute(QWebEngineSettings::PluginsEnabled, true); disabled by default, not required (usefull for Pepper Plugin API.)
     settings()->setAttribute(QWebEngineSettings::JavascriptCanOpenWindows, true);
+//--new
+    settings()->setAttribute(QWebEngineSettings::JavascriptCanAccessClipboard, true);
+    settings()->setAttribute(QWebEngineSettings::JavascriptCanPaste, true); //depends on^
+    settings()->setAttribute(QWebEngineSettings::JavascriptEnabled, true);
+    settings()->setAttribute(QWebEngineSettings::SpatialNavigationEnabled, true);
+    settings()->setAttribute(QWebEngineSettings::ScrollAnimatorEnabled, true);
+    settings()->setAttribute(QWebEngineSettings::TouchIconsEnabled, true); //test
+    //settings()->setAttribute(QWebEngineSettings::AllowRunningInsecureContent, true); default false, https everywhere on https pages
+    //settings()->setAttribute(QWebEngineSettings::AllowGeolocationOnInsecureOrigins, false); //default
+    settings()->setAttribute(QWebEngineSettings::AllowWindowActivationFromJavaScript, true);
+    settings()->setAttribute(QWebEngineSettings::ShowScrollBars, true);
+    settings()->setAttribute(QWebEngineSettings::PlaybackRequiresUserGesture, false); //default true, like a mobile browser
+    //settings()->setAttribute(QWebEngineSettings::WebRTCPublicInterfacesOnly, false); //default, true is more secure
+    settings()->setAttribute(QWebEngineSettings::DnsPrefetchEnabled, true); //default disabled
+    //settings()->setAttribute(QWebEngineSettings::PdfViewerEnabled, true); //default
+    settings()->setFontSize(QWebEngineSettings::MinimumFontSize,12);
+    settings()->setFontSize(QWebEngineSettings::MinimumLogicalFontSize,12);
+    settings()->setFontSize(QWebEngineSettings::DefaultFontSize,24);
+    settings()->setFontSize(QWebEngineSettings::DefaultFixedFontSize,26);
+    settings()->setUnknownUrlSchemePolicy(QWebEngineSettings::AllowAllUnknownUrlSchemes);
+//https://doc.qt.io/qt-5/qwebenginesettings.html#WebAttribute-enum
+//++new
     settings()->setAttribute(QWebEngineSettings::WebGLEnabled, true);
     settings()->setAttribute(QWebEngineSettings::Accelerated2dCanvasEnabled, true);
     this->setAllPaths(*pwd);
@@ -60,8 +83,11 @@ void WebEngineView::showContextMenu(const QPoint &pos){
 	return;
     QMenu *contextMenu = new QMenu();
     QAction *return_link = new QAction(tr("Return Link"));
+    QAction *sysopen_link = new QAction(tr("Sys Open Link"));
     connect(return_link, &QAction::triggered, this, [this] {setUrl(QUrl(linkurl));ExitAction();});
+    connect(sysopen_link, &QAction::triggered, this, [this] {QDesktopServices::openUrl(QUrl(linkurl));});
     contextMenu->addAction(return_link);
+    contextMenu->addAction(sysopen_link);
     contextMenu->exec(this->mapToGlobal(pos));
 }
 
